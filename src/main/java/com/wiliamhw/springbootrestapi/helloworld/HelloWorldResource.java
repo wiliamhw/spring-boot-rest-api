7291c576c8778530848673cfe26b23ac3dbe9eef
William H.W <wiliamwijaya1985@gmail.com>
2023-09-06 21:15:31 +0700

    feat: handle path parameter

--- src/main/java/com/wiliamhw/springbootrestapi/helloworld/HelloWorldResource.java
+++ src/main/java/com/wiliamhw/springbootrestapi/helloworld/HelloWorldResource.java
@@ -1,5 +1,6 @@
 package com.wiliamhw.springbootrestapi.helloworld;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +15,10 @@ public class HelloWorldResource {
     @RequestMapping("/hello-world-bean")
     public HelloWorldBean helloWordBean() {
         return new HelloWorldBean("Hello World");
+    }
+
+    @RequestMapping("/hello-world-path-param/{name}/message/{message}")
+    public HelloWorldBean helloWordBeanMultiplePathParam(@PathVariable String name, @PathVariable String message) {
+        return new HelloWorldBean("Hello World " + name + ", " + message);
     }
 }
